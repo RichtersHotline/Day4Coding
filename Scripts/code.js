bfa786799254d4f77dbd2267de617d0ce33167fd
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-17 18:03:36 +0200

    added scripts

---
+++ Scripts/code.js
@@ -0,0 +1,9 @@
+const person = {firstname:"Johnny", age:29, eyeColor:"green"};
+
+const person2 = {firstname:"Brienne", age:39, eyeColor:"brown"};
+console.log(person)
+console.log(person2)
+
+let people = [person, person2]
+
+console.log(people)
